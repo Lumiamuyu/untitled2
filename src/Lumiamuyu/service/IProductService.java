4026d3f163f59bad1c4805d63bd47206a7d6344a
Lumiamuyu <lumiamuyu@live.com>
2018-12-14 10:50:27 +0800

    Initial commit

---
+++ src/Lumiamuyu/service/IProductService.java
@@ -0,0 +1,13 @@
+package Lumiamuyu.service;
+
+import Lumiamuyu.pojo.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    public List<Product> getLists();
+    public int add(Product product);
+    public int dele(int id);
+    public Product getOne(int id);
+    public int update(Product product);
+}
